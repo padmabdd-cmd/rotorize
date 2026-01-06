66ea113fde2dafb75a406af36cf81fbf7640140d
padmabdd-lgtm <padmabdd@gmail.com>
2026-01-06 22:05:08 +0530

    Fix model import case sensitivity for Render

--- controllers/controllerApplication.js
+++ controllers/controllerApplication.js
@@ -1,4 +1,4 @@
-const Application = require("../models/application");
+const Application = require("../models/Application");
 const nodemailer = require("nodemailer");
 const path = require("path");
 const PDFDocument = require("pdfkit");
